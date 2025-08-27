33940270486b439f09ab355570963a983498eaea
Talhao4Shahid <talha.shahid@kryptomind.com>
2025-08-27 16:29:31 +0500

    create a new app and delete the previous app

--- libs/common/src/database/abstract.repository.ts
+++ libs/common/src/database/abstract.repository.ts
@@ -1,6 +1,6 @@
-import { Logger } from '@nestjs/common';
+import { Logger, NotFoundException } from '@nestjs/common';
 import { AbstractDocument } from './abstract.schema';
-import { Model, Types } from 'mongoose';
+import { FilterQuery, Model, Types, UpdateQuery } from 'mongoose';
 
 export abstract class AbstractRepository<TDocument extends AbstractDocument> {
   protected abstract readonly logger: Logger;
@@ -13,5 +13,50 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
       _id: new Types.ObjectId(), // Ensure _id is set to a new ObjectId
     });
     return (await createdDocument.save()).toJSON() as unknown as TDocument;
+  }
+
+  async findOne(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
+    const document = await this.model.findOne(filterQuery).lean(true);
+    if (!document) {
+      this.logger.warn(
+        `Document not found with filter: ${JSON.stringify(filterQuery)}`,
+      );
+      throw new NotFoundException('Document not found');
+    }
+
+    return document as TDocument;
+  }
+
+  async findOneAndUpdate(
+    filterQuery: FilterQuery<TDocument>,
+    update: UpdateQuery<TDocument>,
+  ): Promise<TDocument> {
+    const document = await this.model
+      .findOneAndUpdate(filterQuery, update, { new: true })
+      .lean<TDocument>(true);
+    if (!document) {
+      this.logger.warn(
+        `Document not found with filter: ${JSON.stringify(filterQuery)}`,
+      );
+      throw new NotFoundException('Document not found');
+    }
+    return document as TDocument;
+  }
+
+  async find(filterQuery: FilterQuery<TDocument>): Promise<TDocument[]> {
+    return this.model.find(filterQuery).lean<TDocument[]>(true);
+  }
+
+  async delete(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
+    const document = await this.model
+      .findByIdAndDelete(filterQuery)
+      .lean<TDocument>(true);
+    if (!document) {
+      this.logger.warn(
+        `Document not found with filter: ${JSON.stringify(filterQuery)}`,
+      );
+      throw new NotFoundException('Document not found');
+    }
+    return document as TDocument;
   }
 }
