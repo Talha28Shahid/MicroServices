2855cd441da2e6af69a091f47ee741008e89d73e
Talhao4Shahid <talha.shahid@kryptomind.com>
2025-09-19 15:33:38 +0500

    implement nodemailer with sendgrid

--- apps/notifications/src/notifications.service.ts
+++ apps/notifications/src/notifications.service.ts
@@ -1,11 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { NotifyEmailDto } from './dto/notify-email.dto';
+import { ConfigService } from '@nestjs/config';
+import * as nodemailer from 'nodemailer';
 
 @Injectable()
 export class NotificationsService {
+  private transporter: nodemailer.Transporter;
+
+  constructor(private readonly configService: ConfigService) {
+    this.transporter = nodemailer.createTransport({
+      host: 'smtp.sendgrid.net',
+      port: 587,
+      secure: false,
+      auth: {
+        user: 'apikey',
+        pass: this.configService.get<string>('SENDGRID_API_KEY'),
+      },
+    });
+  }
+
   async sendEmail(data: NotifyEmailDto) {
-    // Simulate sending an email
-    console.log(`Sending email to ${data.email}`);
-    return { success: true };
+    await this.transporter.sendMail({
+      from: 'talhashahid392392@gmail.com',
+      to: 'Talhashahid392392@outlook.com',
+      subject: 'Welcome to Our App!',
+      text: 'Thanks for reservation ',
+      html: '<h1>Welcome!</h1><p>Thanks for joining us.</p>',
+    });
   }
 }
